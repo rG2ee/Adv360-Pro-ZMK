dc3e2af41e71f4ccbbc827b613380635fa695b36
user <user@321.de>
2024-05-17 09:25:20 +0200

    devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "script executed" > /home/user/status.txt
+# Starting Tailscale daemon automatically if not running.
+
+# Überprüft, ob der Tailscale-Daemon läuft und gibt das Ergebnis aus.
+if pgrep -x "tailscaled" > /dev/null; then
+  echo "TailscaleD-Status: Running" >> /home/user/status.txt
+  RUNNING=true
+else
+  echo "TailscaleD-Status: Not running" >> /home/user/status.txt
+  RUNNING=false
+fi
+
+if [ "$RUNNING" = false ]; then
+    sudo tailscaled > /dev/null 2>&1 &
+    disown
+    echo "tailscaled has been started" >> /home/user/status.txt
+    sleep 5
+fi
+
+source /home/user/.env.tailscale
+sudo tailscale up --authkey $TAIL_SCALE_AUTH_KEY --hostname=devcontainer-`hostname`  > /home/user/log.txt 2>&1
+sudo rm /home/user/.env.tailscale
+sudo rm /home/user/postCreateCommand.sh
